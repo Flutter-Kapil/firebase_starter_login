017ea83d90ef3f81d3876f723158207f66b2c6ae
kapil jhajhria <jhajhria44@gmail.com>
2019-10-25 10:21:03 +0530

    Round edges flat button

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -12,37 +12,38 @@ class _LandingPageState extends State<LandingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: FractionallySizedBox(
-                  widthFactor: 0.5, child: Image.asset('assets/logo.png')),
-            ),
-            Text(
-              'McLaren Chat',
-              style: TextStyle(color: Colors.blue, fontSize: 30),
-              textAlign: TextAlign.center,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: FlatButton(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: FractionallySizedBox(
+                    widthFactor: 0.5, child: Image.asset('assets/logo.png')),
+              ),
+              Text(
+                'McLaren Chat',
+                style: TextStyle(color: Colors.blue, fontSize: 30),
+                textAlign: TextAlign.center,
+              ),
+              FlatButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0)),
                 color: Colors.blue,
                 child: Text(
                   'Login',
-                  style: TextStyle(fontSize: 20, color: Colors.white),
+                  style: TextStyle(fontSize: 21, color: Colors.white),
                 ),
                 onPressed: () {
                   Navigator.push((context),
                       MaterialPageRoute(builder: (context) => SignInPage()));
                 },
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 8, right: 8),
-              child: FlatButton(
+              FlatButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0)),
                 color: Colors.purple,
                 onPressed: () {
                   Navigator.push((context),
@@ -50,11 +51,11 @@ class _LandingPageState extends State<LandingPage> {
                 },
                 child: Text(
                   'Register',
-                  style: TextStyle(fontSize: 20, color: Colors.white),
+                  style: TextStyle(fontSize: 21, color: Colors.white),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
